4148efb2f5b5cb08e074199ee3f03e434e44d42a
Alexander Brandes <brandes.alexander@web.de>
2023-06-28 16:36:07 +0200

    Update Jenkinsfile syntax (#4)
    * Update Jenkinsfile syntax
    
    * Switch repo to https
    
    * Update src/test/java/org/jenkins_cli/plugins/ifdtms/CucumberPostBuildTest.java
    
    Co-authored-by: Basil Crow <me@basilcrow.com>
    
    ---------
    
    Co-authored-by: Basil Crow <me@basilcrow.com>

--- src/test/java/org/jenkins_cli/plugins/ifdtms/CucumberPostBuildTest.java
+++ src/test/java/org/jenkins_cli/plugins/ifdtms/CucumberPostBuildTest.java
@@ -2,10 +2,12 @@ package org.jenkins_cli.plugins.ifdtms;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CucumberPostBuildTest {
 
+	@Ignore
 	@Test
 	public void test() {
 		fail("Not yet implemented");
